79978e06b3c1aaf5fa5c7a04c2c83cb9d9dd96f4
kirillpyatnitskiy <Stbskgt194i>
2021-01-12 11:31:32 +0300

    refactor

--- src/Main.kt
+++ src/Main.kt
@@ -88,7 +88,6 @@ fun testMap(){
 
 fun testMapOf(){
     val map : Map<String,String> = mapOf("first" to "Один", "second" to "Два")
-    val str = map.getValue("first")
     println(map.getValue("first"))
     println()
 }
